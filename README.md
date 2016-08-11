12ca26a9868ea0edd780924db789fdfa3a47003d
Goran Mahovlic <goran.mahovlic@gmail.com>
2016-08-11 23:26:58 +0200

    Adding support for LightStrip

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 # 
 
 Get wireless signal and show it on RGB led strip
+Tested and working
+
+You will need to add you SSID and password
+Pin on ESP is now set to D1
+Change NUM_LEDS (how many leds you have)
+
+Do not connect RBG strip while EsP is connected to USB port I dod not test it that way it could draw to nuch current.
+
+Im using one Li-ion cell to power ESP and strip... 
--- WifiSignalPainting/WifiSignalPainting.ino
+++ WifiSignalPainting/WifiSignalPainting.ino
@@ -1,18 +1,35 @@
 /*
    ESP8266 WiFi light painting stick
+   For Strip you will need
+   http://fastled.io library from
+   https://github.com/FastLED/FastLED
 */
+
+#include "FastLED.h"
 #include <ESP8266WiFi.h>
 
-//Enter your wifi data below
+// Define pin on ESP that will be connected to strip
+#define DATA_PIN 1
+
+// Define no of leds you have on strip
+#define NUM_LEDS 10
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+// Enter your wifi data below
 const char* myssid = "Your wifi SSID";
 const char* mypass = "Your wifi password";
 
-//We will store signal data in this varible
+// We will store signal data in this varible
 long rssi;
+int signalStrenght;
 
 void setup() {
   //Starting serial for debuging purpose
   Serial.begin(115200);
+
+  FastLED.addLeds<WS2811, DATA_PIN, GRB>(leds, NUM_LEDS);
 
   Serial.println("\nESP8266 WiFi light painting stick");
 
@@ -39,13 +56,52 @@ void setup() {
 }
 
 void loop() {
-
   //Get signal strenght
   rssi = WiFi.RSSI();  // eg. -63
   //Print signal strenght (not mapped)
   Serial.println(rssi);
   //Print signal strenght mapped from 0-90 for easier usage
-  Serial.println(map(rssi, -100, 0, 0, 90));
-  //Wait for 1sec
-  delay(1000);
+  signalStrenght = int(map(rssi, -100, -50, 1, NUM_LEDS));
+    int colorSegments = NUM_LEDS / 5 ;
+  Serial.println(signalStrenght);
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    int setColors = i / colorSegments ;
+    if (setColors < 1)
+    {
+      if (signalStrenght >= i)
+        leds[i] = CRGB::Red;
+      else
+        leds[i] = CRGB::Black;
+    }
+    else if  (setColors >= 1 && setColors < 2)
+    {
+      if (signalStrenght >= i)
+        leds[i] = CRGB::GreenYellow;
+      else
+        leds[i] = CRGB::Black;
+    }
+    else if  (setColors >= 2 && setColors < 3)
+    {
+      if (signalStrenght >= i)
+        leds[i] = CRGB::Orange;
+      else
+        leds[i] = CRGB::Black;
+    }
+    else if  (setColors >= 3 && setColors < 4)
+    {
+      if (signalStrenght >= i)
+        leds[i] = CRGB::Blue;
+      else
+        leds[i] = CRGB::Black;
+    }
+    else if  (setColors >= 4)
+    {
+      if (signalStrenght >= i)
+        leds[i] = CRGB::Green;
+      else
+        leds[i] = CRGB::Black;
+    }
+  }
+  FastLED.show();
 }
